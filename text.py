175ed354104ceda3556f0e011d40a177946c0663
mijganello <90396002+mijganello@users.noreply.github.com>
2022-01-22 11:16:13 +0500

    Update text.py

--- text.py
+++ text.py
@@ -1,27 +1,79 @@
-file = open('text.txt')
-text1 = file.read()
-file = open('text.txt')
-text = file.readlines()
+class Book:
 
-def counter(txt,txt1):
-    line_count = 0
-    word_count = 0
-    punct_counter = 0
-    puncts = ['.', ',', '?', '!', ';', ':', '-', '’','[', ']', '‒', '–', '—', '―', '...','“', '”', '‘', '’', '«', '»', '‹', '›']
-    
-    for x in txt:
-        line_count+=1
-    
-    word_count = len(txt1.split())
+def __init__(self, name, author):
 
-    for x in txt1:
-        if x in puncts:
-            punct_counter+=1
+self.name = name
 
-    
-    return(line_count, word_count, punct_counter)
-    
+self.author = author
 
-result = counter(text,text1)
-print(result)
+class Bookshelf:
 
+def __init__(self, capacity=20):
+
+self.capacity = capacity
+
+self.names = []
+
+self.authors = []
+
+def add_book(self, book):
+
+if len(self.names) <= self.capacity:
+
+self.names.append(book.name)
+
+self.authors.append(book.author)
+
+print("Книга успешно добавлена")
+
+else:
+
+raise Exception("На полке нет места")
+
+def delete_book(self, book):
+
+for x in self.names:
+
+if x == book.name:
+
+self.names.remove(x)
+
+self.authors.remove(x)
+
+else:
+
+raise Exception("Книга с таким названием и автором не была найдена")
+
+def find_book(self, book):
+
+for x in self.names:
+
+if x == book.name:
+
+print("Книга найдена на полке")
+
+else:
+
+raise Exception("Книга с таким названием и автором не была найдена")
+
+#def show_book(self):
+
+# for x in self.names
+
+shelf = Bookshelf()
+
+book1 = Book("МертвыеДуши", "Гоголь")
+
+book2 = Book("Обломов", "Грибоедов")
+
+book3 = Book("Меч предназначения", "Сапковский")
+
+book4 = Book("Пылающая роза", "Сапковский")
+
+shelf.add_book(book1)
+
+shelf.add_book(book2)
+
+shelf.add_book(book3)
+
+shelf.add_book(book4)
